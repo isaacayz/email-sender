ba4d85422e1a601211658c41e7c94b1684e712ae
Isaac Ige <heywhydot16@gmail.com>
2023-06-19 17:31:14 +0100

    changed some def to properties. dependency injection

--- class.py
+++ class.py
@@ -21,13 +21,29 @@ class Address:
 
 @dataclass
 class Stats:
-    height: str
+    height: float
     age: int
     gender: str
-    weight: str
+    weight: float
     hair_color: str
     eye_color: str
     blood_type: str
+
+    @property
+    def bmi(self) -> float:
+        return self.weight / (self.height ** 2)
+    
+    @property
+    def bmi_category(self) -> str:
+        if self.bmi < 18:
+            return 'You are kinda underweight bruh'
+        elif self.bmi < 30:
+            return 'You seem normal to me'
+        elif self.bmi > 40:
+            return "You are kinda overweight bruh"
+        else: 
+            return 'Iono what your case is...'
+        
 
 @dataclass
 class Person:
@@ -63,10 +79,10 @@ def main() -> None:
         country='NGN'
     )
     stats = Stats(
-        height='185cm',
+        height=1.85,
         age=12,
         gender='Female',
-        weight='56kg',
+        weight=56,
         hair_color='brown',
         eye_color='red',
         blood_type='O+'
@@ -79,6 +95,9 @@ def main() -> None:
         address=address
     )
 
+    bmi_value = stats.bmi
+    print(f"Your bmi value is {bmi_value:.2f}")
+    print(f"You fall under this bmi category: {stats.bmi_category}")
     person.update_email("isaacayz@live.com")
 
 if __name__ == "__main__":
